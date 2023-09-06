f6acd4cdfeafecae1ceecbf166ef908a33a357b9
Wistaster <wistaster@hotmail.com>
2023-09-06 21:17:17 +0300

    Overdesign!

---
+++ SQLQuery.sql
@@ -0,0 +1,7 @@
+﻿select p.ProductName, sum(od.UnitPrice*od.Quantity) as total from products p
+inner join [Order Details] od on p.ProductID=od.ProductID
+group by p.ProductName
+
+select * from Orders
+select * from Products
+select * from [Order Details]
